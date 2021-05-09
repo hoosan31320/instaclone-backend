d8b508dbe783acbd454ac75b604b465987ec3d83
hoosan31320 <hoos31320@gmail.com>
2021-05-10 03:20:25 +0700

    Authentication 3

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
+import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
     schema,
-    context: {
-        token: 
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjIwNTAxMzU2fQ.qysQXNV34v1WL7_nCBxxutD1bMsRVYqHO4FxglFJk4Y"
-    }
+    context: async ({ req }) => {
+        return {
+          loggedInUser: await getUser(req.headers.token),
+        };
+    },
 });
 
 server
