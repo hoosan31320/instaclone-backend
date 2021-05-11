640e7a90921bc8fc9de6011a2dd10a9e089129b9
hoosan31320 <hoos31320@gmail.com>
2021-05-12 04:44:21 +0700

    Searching Users

---
+++ users/searchUsers/searchUsers.resolvers.js
@@ -0,0 +1,11 @@
+import client from "../../client";
+
+export default {
+    Query: {
+        searchUsers: async (_, { keyword }) =>
+            await client.user.findMany({
+                where: { username: { startsWith: keyword.toLowerCase() } }
+                // where: { username: { contains: keyword.toLowerCase() } }
+            })
+    }
+};
