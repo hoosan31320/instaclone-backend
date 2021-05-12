f9657337110db545d174eed4017683f8b7fae60b
hoosan31320 <hoos31320@gmail.com>
2021-05-12 16:18:56 +0700

    3.6 seeHashtag

--- photos/photos.resolvers.js
+++ photos/photos.resolvers.js
@@ -7,5 +7,16 @@ export default {
             client.hashtags.findMany({
                 where: { photos: { some: { id } } }
             })
+    },
+    Hashtag: {
+        photos: ({ id }, { page }, { loggedInUser }) => {
+            return client.hashtag
+                .findUnique({ where: { id } })
+                .photos();
+        },
+        totalPhotos: ({ id }) =>
+            client.photo.count({
+                where: { hashtags: { some: { id } } }
+            })
     }
 };
