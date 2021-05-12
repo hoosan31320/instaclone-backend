b1d1ae4b105d953c7fa641c4c9c0d8ae1a1c53de
hoosan31320 <hoos31320@gmail.com>
2021-05-12 15:03:50 +0700

    3.3 Upload Photo 2

--- photos/uploadPhoto/uploadPhoto.resolvers.js
+++ photos/uploadPhoto/uploadPhoto.resolvers.js
@@ -1,3 +1,4 @@
+import client from "../../client";
 import { protectedResolver } from "../../users/users.utils";
 
 export default {
@@ -7,9 +8,23 @@ export default {
                 if (caption) {
                     /// parse caption
                     // get or create Hashtags
+                    const hashtags = caption.match(/#[\w]+/g);
                 }
                 // save the photo With the parsed hashtags
                 // add the photo to the hashtags
+                client.photo.create({
+                    data: { 
+                        file, 
+                        caption, 
+                        hashtags: {
+                            connectOrCreate: [
+                                { where: { hashtags: "#food" },
+                                  create: { hashtags: "#food" }
+                                }
+                            ]
+                        }
+                    }
+                })
             }
         )
     }
