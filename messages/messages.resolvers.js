37059100861829190ec1698e44736bd4dea9d37e
hoosan31320 <hoos31320@gmail.com>
2021-05-20 20:13:05 +0700

    4.5 readMessage

--- messages/messages.resolvers.js
+++ messages/messages.resolvers.js
@@ -6,5 +6,16 @@ export default {
         messages: ({ id }) => client.message.findMany({
             where: { roomId: id },
         }),
+        unreadTotal: ({ id }, _, { loggedInUser }) => {
+            if (!loggedInUser) {
+                return 0;
+            }
+            return client.message.count({
+                where: { read: false,
+                         roomId: id,
+                         user: { id: { not: loggedInUser.id } }
+                       }
+            })
+        }
     }
 }
