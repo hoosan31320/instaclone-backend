95db82abd23286f67f243e434da2ecfd652b2896
hoosan31320 <hoos31320@gmail.com>
2021-06-22 14:06:04 +0700

    server.js inside the folder build

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 import http from "http";
 import express from "express";
 import logger from "morgan";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs, resolvers } from "./schema";
 import { getUser } from "./users/users.utils";
+
+dotenv.config();
 
 const PORT = process.env.PORT;
 const apollo = new ApolloServer({
