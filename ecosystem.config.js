428a692a20d59d5092f7cba92c36efe6364872a8
hoosan A <hoos31320@gmail.com>
2021-07-12 01:55:56 +0700

    Fork mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "instaclone", // pm2 name
       script: "./build/server-register.js", // // 앱 실행 스크립트
-      instances: 2, // 클러스터 모드 사용 시 생성할 인스턴스 수
+      // instances: 1, // 클러스터 모드 사용 시 생성할 인스턴스 수
       exec_mode: "fork", // fork, cluster 모드 중 선택
       merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
       autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
