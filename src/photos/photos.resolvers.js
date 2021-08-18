98a8a64f006c664d03ea4ad73d27a5bd6d5ff911
hoosan A <hoos31320@gmail.com>
2021-08-19 00:07:23 +0700

    add seeFeedFinalNo

--- src/photos/photos.resolvers.js
+++ src/photos/photos.resolvers.js
@@ -2,60 +2,60 @@ import client from "../client";
 
 export default {
   Photo: {
-      user: ({ userId }) => client.user.findUnique({ where: { id: userId } }),
-      hashtags: ({ id }) =>
-        client.hashtag.findMany({
-            where: { photos: { some: { id } } }
-        }),
-      likes: ({ id }) => client.like.count({ where: { photoId: id } }),
-      commentNumber: ({ id }) => client.comment.count({ where: { photoId: id } }),
-      comments: ({ id }) => 
-        client.comment.findMany({
-          where: { photoId: id },
-          include: { user: true }
-        }),
-      isMine: ({ userId }, _, { loggedInUser }) => {
-        if (!loggedInUser) {
-          return false;
-        }
-        return userId === loggedInUser.id;
-      },
-      isLiked: async ({ id }, _, { loggedInUser }) => {
-        if (!loggedInUser) {
-          return false;
-        }
-        const ok = await client.like.findUnique({
-          where: { photoId_userId: { photoId: id, userId: loggedInUser.id } },
-          select: { id: true }
-        })
-        if (ok) {
-          return true;
-        }
+    user: ({ userId }) => client.user.findUnique({ where: { id: userId } }),
+    hashtags: ({ id }) =>
+      client.hashtag.findMany({
+          where: { photos: { some: { id } } }
+      }),
+    likes: ({ id }) => client.like.count({ where: { photoId: id } }),
+    commentNumber: ({ id }) => client.comment.count({ where: { photoId: id } }),
+    comments: ({ id }) => 
+      client.comment.findMany({
+        where: { photoId: id },
+        include: { user: true }
+      }),
+    isMine: ({ userId }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
         return false;
-      },
-      seeFeedFinalNo: async (_, { offset }, { loggedInUser}) => {
-        const { following } = await client.user.findUnique({
-          where: { id: loggedInUser.id },
-          select: { following: true }
-        });
-        const seeFeedPhotos = await client.photo.findMany({
-          where: { 
-            user : { 
-              id: {
-                in: [...following.map(user => user.id), loggedInUser.id ]
-              } 
-            }
-          }, 
-        });
-        const seeFeedFinalNo = [];
-        for (var item of seeFeedPhotos) {
-          const seeFeedPhotoIds = item.id;
-          seeFeedFinalNo.push(seeFeedPhotoIds);
-        }
-        const minValue = Math.min(...seeFeedFinalNo);
+      }
+      return userId === loggedInUser.id;
+    },
+    isLiked: async ({ id }, _, { loggedInUser }) => {
+      if (!loggedInUser) {
+        return false;
+      }
+      const ok = await client.like.findUnique({
+        where: { photoId_userId: { photoId: id, userId: loggedInUser.id } },
+        select: { id: true }
+      })
+      if (ok) {
+        return true;
+      }
+      return false;
+    },
+    seeFeedFinalNo: async (_, { offset }, { loggedInUser}) => {
+      const { following } = await client.user.findUnique({
+        where: { id: loggedInUser.id },
+        select: { following: true }
+      });
+      const seeFeedPhotos = await client.photo.findMany({
+        where: { 
+          user : { 
+            id: {
+              in: [...following.map(user => user.id), loggedInUser.id ]
+            } 
+          }
+        }, 
+      });
+      const seeFeedFinalNo = [];
+      for (var item of seeFeedPhotos) {
+        const seeFeedPhotoIds = item.id;
+        seeFeedFinalNo.push(seeFeedPhotoIds);
+      }
+      const minValue = Math.min(...seeFeedFinalNo);
 
-        return minValue;
-		  }
+      return minValue;
+    }
   },
   Hashtag: {
     photos: ({ id }, { page }, { loggedInUser }) => {
