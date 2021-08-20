7855e18b92d31f3752f5a8fefa53ceb611c7da65
hoosan A <hoos31320@gmail.com>
2021-08-20 23:23:56 +0700

    searchPhoto take:20

--- src/photos/searchPhotos/searchPhotos.resolvers.js
+++ src/photos/searchPhotos/searchPhotos.resolvers.js
@@ -4,7 +4,7 @@ export default {
   Query: {
     searchPhotos: (_, { offset, keyword }) => {
       return client.photo.findMany({
-        take: 15,
+        take: 20,
         skip: offset,
         where: { caption: { contains: keyword } },
         // where: { caption: { startsWith: keyword } }
