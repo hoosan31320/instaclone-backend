c167e9d31fe278b3b63e0c54865e0dd06a70378d
hoosan31320 <hoos31320@gmail.com>
2022-06-18 23:40:51 +0700

    user 1번 추가

--- src/photos/seeFeed/seeFeed.resolvers.js
+++ src/photos/seeFeed/seeFeed.resolvers.js
@@ -5,13 +5,11 @@ export default {
   Query: {
     seeFeed: protectedResolver( async (_, { offset }, { loggedInUser }) => {
       const { following } = await client.user.findUnique({
-        where: { id: loggedInUser.id },
+        where: { id: loggedInUser.id, id: 1 },
         select: { following: true }
       });
       // https://github.com/JoonDong2/instagureng-backend/blob/master/src/api/Post/seeFeed/seeFeed.js
-      const photoFirst = { photoId: 1 } ;
-
-      return (following == null) ? photoFirst : client.photo.findMany({
+      return client.photo.findMany({
         take: 30,
         skip: offset,
         where: { 
