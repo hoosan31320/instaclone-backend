74bbe2d507e77b76df1da3d66162bb7a2b3087b4
hoosan31320 <hoos31320@gmail.com>
2022-06-20 05:13:57 +0700

    add room Resolver to UserResolver

--- src/users/users.resolvers.js
+++ src/users/users.resolvers.js
@@ -28,5 +28,6 @@ export default {
       return Boolean(exists);
     },
     photos: ({ id }) => client.user.findUnique({ where : { id } }).photos()
+    rooms: ({ id }) => client.room.findUnique({ where: { id } }).users()
   }
 };
